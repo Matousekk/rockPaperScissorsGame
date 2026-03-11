979e5e912d37ababe3cb753a99937100d397d9c9
Tomas Matousek <tomas.matousekst@gmail.com>
2026-03-11 20:46:01 +0100

    UI design rehaul, more JS code

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const images = document.querySelectorAll('img');
 
 images.forEach(img => {
     img.addEventListener('click', () => {
+        document.querySelector('#roundText').innerHTML = '';
         playRound(img);
     });
 });
@@ -26,14 +27,14 @@ function evaluateNumber(computerNumber) {
 //evaluate who is the winner
 function evaluateWinner(humanChoice, computerChoice) {
     if(humanChoice === "rock" && computerChoice === "scissors") {
-        return "YOU WIN!"
+        document.querySelector('#roundText').appendChild(roundWon)
     }  else if(humanChoice === "paper" && computerChoice === "rock") {
-        return "YOU WIN!"
+        document.querySelector('#roundText').appendChild(roundWon)
     }  else if(humanChoice === "scissors" && computerChoice === "paper") {
-        return "YOU WIN!"
+        document.querySelector('#roundText').appendChild(roundWon)
     }  else if(humanChoice === computerChoice) {
-        return "IT'S A TIE!"
-    }  else {return "YOU LOST!"}
+        return document.querySelector('#roundText').appendChild(roundTie)
+    }  else {document.querySelector('#roundText').appendChild(roundLost)}
 };
 
 //plays a round, evaluates winner
@@ -44,3 +45,39 @@ function playRound(humanChoice) {
     const victory = evaluateWinner(humanChoice, computerChoice);
     console.log(victory);
 }
+
+
+//assign score and rounds number placeholders
+//const scoreImg = document.querySelector('score');
+//const roundImg = document.querySelector('rounds');
+
+//elements for score and number images
+const numberZero = document.createElement('img');
+numberZero.src = '/img/zero.png';
+//displaying score 0 by default
+document.querySelector('#score').appendChild(numberZero);
+document.querySelector('#rounds').appendChild(numberZero.cloneNode());
+
+const numberOne = document.createElement('img');
+numberOne.src = '/img/number-1.png';
+
+const numberTwo = document.createElement('img');
+numberTwo.src = '/img/number-2.png';
+
+const numberThree = document.createElement('img');
+numberThree.src = '/img/number-3.png';
+
+const numberFour = document.createElement('img');
+numberFour.src = '/img/number-4.png';
+
+const numberFive = document.createElement('img');
+numberFive.src = '/img/number-5.png';
+
+const roundLost = document.createElement('h1');
+roundLost.textContent = 'ROUND LOST';
+
+const roundWon = document.createElement('h1');
+roundWon.textContent = 'ROUND WON';
+
+const roundTie = document.createElement('h1');
+roundTie.textContent = `IT'S A TIE`;
