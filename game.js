e6a38bc168d61046cd56bd5927191f0516d01ee7
Tomas Matousek <tomas.matousekst@gmail.com>
2026-03-13 13:06:42 +0100

    Final changes, round reset button, score tracking JS

--- game.js
+++ game.js
@@ -1,10 +1,15 @@
 //play buttons
 const images = document.querySelectorAll('img');
 const rounds = document.querySelector('#rounds');
+const score = document.querySelector('#score');
 
 //match button
 const matchBtn = document.querySelector('#userButtonTwo');
 matchBtn.textContent = 'Reset score';
+matchBtn.addEventListener('click', () => {
+    score.innerHTML = '';
+    score.appendChild(numberZero);
+});
 
 //Event listener for each image
 images.forEach(img => {
@@ -14,7 +19,6 @@ images.forEach(img => {
         roundNumber += 1;
         displayRoundNumber();
         scoreTracker(playerScore, computerScore);
-        console.log(roundNumber);
     });
 });
 
@@ -74,17 +78,21 @@ function evaluateWinner(humanChoice, computerChoice) {
     if(humanChoice === "rock" && computerChoice === "scissors") {
         document.querySelector('#roundText').appendChild(roundWon);
         playerScore += 1;
+        computerScore -= 1;
     }  else if(humanChoice === "paper" && computerChoice === "rock") {
         document.querySelector('#roundText').appendChild(roundWon);
         playerScore += 1;
+        computerScore -= 1;
     }  else if(humanChoice === "scissors" && computerChoice === "paper") {
         document.querySelector('#roundText').appendChild(roundWon);
         playerScore += 1;
+        computerScore -= 1;
     }  else if(humanChoice === computerChoice) {
         return document.querySelector('#roundText').appendChild(roundTie);
     }  else {
         document.querySelector('#roundText').appendChild(roundLost);
         computerScore += 1;
+        playerScore -= 1;
     };
 };
 
@@ -100,7 +108,7 @@ const numberZero = document.createElement('img');
 numberZero.src = '/img/zero.png';
 
 //displaying score 0 by default
-document.querySelector('#score').appendChild(numberZero);
+score.appendChild(numberZero);
 rounds.appendChild(numberZero.cloneNode());
 
 //img elements
@@ -118,6 +126,12 @@ numberFour.src = '/img/number-4.png';
 
 const numberFive = document.createElement('img');
 numberFive.src = '/img/number-5.png';
+
+const numberMinusOne = document.createElement('img');
+numberMinusOne.src = '/img/numberOne.png';
+
+const numberMinusTwo = document.createElement('img');
+numberMinusTwo.src = '/img/numberTwo.png';
 
 const roundLost = document.createElement('h1');
 roundLost.textContent = 'ROUND LOST';
@@ -138,8 +152,16 @@ let computerScore = 0;
 //add score based on rounds
 function scoreTracker(playerWins, computerWins) {
     if (playerWins == 5) {
-        console.log('addScore!');
+        score.innerHTML = '';
+        score.appendChild(numberOne.cloneNode());
+    }   else if (playerWins == 10) {
+        score.innerHTML = '';
+        score.appendChild(numberTwo.cloneNode());
     }   else if (computerWins == 5) {
-        console.log('decrementScore');
+        score.innerHTML = '';
+        score.appendChild(numberMinusOne);
+    }   else if (computerWins == 10) {
+        score.innerHTML = '';
+        score.appendChild(numberMinusTwo);
     }
 };
